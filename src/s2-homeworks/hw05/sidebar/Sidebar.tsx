83a33cde7cfefa19d0e1bd488772c9d495a10625
Igor <filinigor@yandex.ru>
2022-10-12 16:48:34 +0300

    HW5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -16,7 +16,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
         <>
             {/*затемнение справа от открытого меню*/}
             {open && <div className={s.background} onClick={handleClose}/>}
-            {open &&
+
             <aside className={sidebarClass}>
                 <button className={s.close} onClick={handleClose}>
                     <img
@@ -56,7 +56,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         Junior Plus
                     </NavLink>
                 </nav>
-            </aside>}
+            </aside>
         </>
     )
 }
