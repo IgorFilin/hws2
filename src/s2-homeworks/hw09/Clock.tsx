fc814b3a766410f04bdb48a65292313ed0b0bee8
Igor <filinigor@yandex.ru>
2022-10-14 17:54:34 +0300

    HW9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -18,6 +18,8 @@ function Clock() {
         setTimerId(+id)
     }
 
+
+
     const stop = () => {
         // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
         clearInterval(timerId)
@@ -40,7 +42,8 @@ function Clock() {
 
     const stringTime = `${getZeroInTime(date.getHours())}:${getZeroInTime(date.getMinutes())}:${getZeroInTime(date.getSeconds())}` ||
         <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = `${getZeroInTime(date.getDay())}.${getZeroInTime(date.getMonth())}.${date.getFullYear()}` ||
+
+    const stringDate = `${date.toLocaleString('default',{day:'numeric'})}.${date.toLocaleString('default',{month:'numeric'})}.${date.getFullYear()}` ||
         <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
