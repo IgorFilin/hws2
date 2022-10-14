6e763ca4e5351b97363465af26cb52f8c1b885f9
Igor <filinigor@yandex.ru>
2022-10-14 15:33:34 +0300

    HW8

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -22,9 +22,9 @@ const initialPeople: UserType[] = [
     {_id: 0, name: 'Кот', age: 3},
     {_id: 1, name: 'Александр', age: 66},
     {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
+    {_id: 3, name: 'Дмитрий', age: 44},
+    {_id: 4, name: 'Ирина', age: 40},
+    {_id: 5, name: 'Виктор', age: 55},
 ]
 
 const HW8 = () => {
